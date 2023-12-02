d9831626bd5a17df400151ace424296bd5f09fb4
sayek ariz <88825949+leakshark@users.noreply.github.com>
2023-12-02 14:12:04 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Sayek',
+        productNumber: 'exe.pdf',
+        paymentStatus: 'Done',
+        status: 'pending'
+    },
+    {
+        productName: 'Monojit',
+        productNumber: 'alo.jpg',
+        paymentStatus: 'Done',
+        status: 'Done'
+    },
+    {
+        productName: 'Farhana',
+        productNumber: 'print.doc',
+        paymentStatus: 'Pending',
+        status: 'pending'
+    },
+]
